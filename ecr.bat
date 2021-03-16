96deda82410dd6a5f7a130959299f99910f871ee
augustoaccorsi <augusto.acorsi@gmail.com>
2021-03-15 23:29:52 -0300

    update ecr and docker run

--- ecr.bat
+++ ecr.bat
@@ -1,11 +1,11 @@
-docker tag microservices_engine:latest 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc:engine
-docker push 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc:engine
+docker tag microservices_nginx:latest 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc/nginx:latest
+docker push 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc/nginx:latest
 
-docker tag microservices_database:latest 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc:database
-docker push 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc:database
+docker tag postgres:latest 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc/database-db:latest
+docker push 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc/database-db:latest
 
-docker tag microservices_nginx:latest 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc:nginx
-docker push 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc:nginx
+docker tag microservices_database:latest 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc/database:latest
+docker push 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc/database:latest
 
-docker tag postgres:latest 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc:postgres
-docker push 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc:postgres
+docker tag microservices_engine:latest 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc/engine:latest
+docker push 571785655335.dkr.ecr.us-east-1.amazonaws.com/augusto-accorsi-tcc/engine:latest
