71d9cc3b8b5d7126b0e8781b8aa2a9d6ed61120b
augustoaccorsi <augusto.acorsi@gmail.com>
2021-03-16 22:30:06 -0300

    change database port

--- database/run.py
+++ database/run.py
@@ -6,6 +6,6 @@ env = os.environ['FLASK_ENV']
 if __name__ == '__main__':
     if env == 'dev':
         db.create_all()
-        app.run(host='0.0.0.0', debug=True)
+        app.run(host='0.0.0.0', port="5001", debug=True)
     else:
-        subprocess.run(["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "wsgi"],debug=True)
+        subprocess.run(["gunicorn", "-w", "4", "-b", "0.0.0.0:5001", "wsgi"],debug=True)
