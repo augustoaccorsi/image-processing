68fcf59d70dba41290aa9b9970e337d18155b8df
augustoaccorsi <augusto.acorsi@gmail.com>
2021-03-18 09:48:27 -0300

    add prod env

--- database/run.py
+++ database/run.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
         db.create_all()
         app.run(host='0.0.0.0', port="5001", debug=True)
     else:
-        subprocess.run(["gunicorn", "-w", "4", "-b", "0.0.0.0:5001", "wsgi"],debug=True)
+        subprocess.run(["gunicorn", "-w", "4", "-b", "0.0.0.0:5001", "wsgi"])
