261e822f3c729421d8bf1abe063ba93c9ff4a958
augustoaccorsi <augusto.acorsi@gmail.com>
2021-03-21 22:32:48 -0300

    travis env  update

---
+++ docker-aws.bat
@@ -0,0 +1,9 @@
+docker-compose build
+docker tag microservices_database 571785655335.dkr.ecr.sa-east-1.amazonaws.com/database
+docker tag microservices_engine 571785655335.dkr.ecr.sa-east-1.amazonaws.com/engine
+docker tag postgres 571785655335.dkr.ecr.sa-east-1.amazonaws.com/postgres
+docker tag microservices_nginx 571785655335.dkr.ecr.sa-east-1.amazonaws.com/nginx
+docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/database
+docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/engine
+docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/nginx
+docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/postgres
--- docker-push.bat
+++ docker-push.bat
@@ -1,4 +1,9 @@
-docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/database
-docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/engine
-docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/nginx
-docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/postgres
+docker-compose build
+docker tag microservices_database augustoaccorsi/database-aws
+docker tag microservices_engine augustoaccorsi/engine-aws
+docker tag postgres augustoaccorsi/postgres-aws
+docker tag microservices_nginx augustoaccorsi/nginx-aws
+docker push augustoaccorsi/database-aws
+docker push augustoaccorsi/postgres-aws
+docker push augustoaccorsi/engine-aws
+docker push augustoaccorsi/nginx-aws
