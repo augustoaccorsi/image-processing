cc81050955d8c1a08d24cc8e628f08d1c8cc3df2
augustoaccorsi <augusto.acorsi@gmail.com>
2021-03-24 20:40:17 -0300

    make some modifications

--- docker-aws.bat
+++ docker-aws.bat
@@ -1,8 +1,8 @@
 docker-compose build
-docker tag microservices_database 571785655335.dkr.ecr.sa-east-1.amazonaws.com/database
-docker tag microservices_engine 571785655335.dkr.ecr.sa-east-1.amazonaws.com/engine
+docker tag database 571785655335.dkr.ecr.sa-east-1.amazonaws.com/database
+docker tag engine 571785655335.dkr.ecr.sa-east-1.amazonaws.com/engine
 docker tag postgres 571785655335.dkr.ecr.sa-east-1.amazonaws.com/postgres
-docker tag microservices_nginx 571785655335.dkr.ecr.sa-east-1.amazonaws.com/nginx
+docker tag nginx 571785655335.dkr.ecr.sa-east-1.amazonaws.com/nginx
 docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/database
 docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/engine
 docker push 571785655335.dkr.ecr.sa-east-1.amazonaws.com/nginx
--- docker-push.bat
+++ docker-push.bat
@@ -1,8 +1,8 @@
 docker-compose build
-docker tag microservices_database augustoaccorsi/database-aws
-docker tag microservices_engine augustoaccorsi/engine-aws
+docker tag database augustoaccorsi/database-aws
+docker tag engine augustoaccorsi/engine-aws
 docker tag postgres augustoaccorsi/postgres-aws
-docker tag microservices_nginx augustoaccorsi/nginx-aws
+docker tag nginx augustoaccorsi/nginx-aws
 docker push augustoaccorsi/database-aws
 docker push augustoaccorsi/postgres-aws
 docker push augustoaccorsi/engine-aws
